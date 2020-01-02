7c0219c8e653e71f380e8ef9fdb79bdb9aedeb7b
nicolahadwen <nicola@ritual.co>
2020-01-02 18:45:57 -0500

    unused import

--- src/main/java/co/hadwen/user/entity/UserEntity.java
+++ src/main/java/co/hadwen/user/entity/UserEntity.java
@@ -15,7 +15,6 @@ import java.util.Random;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
